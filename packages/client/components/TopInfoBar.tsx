629a2136362fa9baf313792ae933f997c9a12f54
Theo Sandell <theo01sandell@gmail.com>
2022-09-14 15:42:21 +0200

    Update TopInfoBar.tsx

--- packages/client/components/TopInfoBar.tsx
+++ packages/client/components/TopInfoBar.tsx
@@ -15,40 +15,44 @@ interface Props {
   settings: GlobalSettings;
 }
 
-const TopInfoBar = forwardRef<HTMLDivElement, Props>(({ settings }, ref) => (
-  <S.Container ref={ref}>
-    <S.inner>
-      <S.InfoItem>
-        <HiOutlineLocationMarker />
-        <BodyNormal>{settings.address}</BodyNormal>
-      </S.InfoItem>
+const TopInfoBar = forwardRef<HTMLDivElement, Props>(({ settings }, ref) => {
+  return (
+    <S.Container ref={ref}>
+      <S.inner>
+        <S.InfoItem>
+          <HiOutlineLocationMarker />
+          <BodyNormal>{settings.address}</BodyNormal>
+        </S.InfoItem>
 
-      <S.InfoItem>
-        <AiOutlineClockCircle />
-        <BodyNormal>{settings.openingHours}</BodyNormal>
-      </S.InfoItem>
+        <S.InfoItem>
+          <AiOutlineClockCircle />
+          <BodyNormal>{settings.openingHours}</BodyNormal>
+        </S.InfoItem>
 
-      <S.InfoItem>
-        <AiOutlinePhone />
-        <a href={`tel:+46${settings.number.replaceAll(" ", "").substring(1)}`}>
-          <BodyNormal>{settings.number}</BodyNormal>
-        </a>
-      </S.InfoItem>
+        <S.InfoItem>
+          <AiOutlinePhone />
+          <a
+            href={`tel:+46${settings.number.replaceAll(" ", "").substring(1)}`}
+          >
+            <BodyNormal>{settings.number}</BodyNormal>
+          </a>
+        </S.InfoItem>
 
-      <S.Social>
-        <S.SocialLink href={settings.facebook}>
-          <AiOutlineFacebook />
-        </S.SocialLink>
+        <S.Social>
+          <S.SocialLink href={settings.facebook}>
+            <AiOutlineFacebook />
+          </S.SocialLink>
 
-        <S.SocialLink href={settings.instagram}>
-          <AiOutlineInstagram />
-        </S.SocialLink>
+          <S.SocialLink href={settings.instagram}>
+            <AiOutlineInstagram />
+          </S.SocialLink>
 
-        <S.SocialLink href={`mailto:${settings.email}`}>
-          <AiOutlineMail />
-        </S.SocialLink>
-      </S.Social>
-    </S.inner>
-  </S.Container>
-));
+          <S.SocialLink href={`mailto:${settings.email}`}>
+            <AiOutlineMail />
+          </S.SocialLink>
+        </S.Social>
+      </S.inner>
+    </S.Container>
+  );
+});
 export default TopInfoBar;
