aa8deeeb56819b5b3de8cd95ee23ba2fc3e2db9c
Theo Sandell <theo01sandell@gmail.com>
2022-09-15 11:53:51 +0200

    Start outputting data on news page

---
+++ packages/client/components/styled/Nyheter.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const CardContainer = styled.div``;
+
+export const Card = styled.div<{ bg: "green" | "white" }>``;
--- packages/client/pages/nyheter.tsx
+++ packages/client/pages/nyheter.tsx
@@ -3,16 +3,50 @@ import Breadcrumbs from "../components/Breadcrumbs";
 import Header from "../components/Header";
 import getGlobalSettings from "../helpers/getGlobalSettings";
 import GlobalSettings from "../types/GlobalSettings";
+import * as S from "../components/styled/Nyheter.styled";
+import Caption from "../components/styled/texts/Caption";
+import HeadingOne from "../components/styled/texts/HeadingOne";
+import client from "../helpers/sanity";
+import HeadingTwo from "../components/styled/texts/HeadingTwo";
+import BodyNormal from "../components/styled/texts/BodyNormal";
 
 interface Props {
   globalSettings: GlobalSettings;
+  data: {
+    title: string;
+    firstCard: {
+      title: string;
+      bodyText: string;
+    };
+    secondCard: {
+      title: string;
+      bodyText: string;
+    };
+  };
 }
 
-const Nyheter: NextPage<Props> = ({ globalSettings }) => {
+const Nyheter: NextPage<Props> = ({ globalSettings, data }) => {
+  console.log(data);
+
   return (
     <>
       <Header settings={globalSettings} />
       <Breadcrumbs />
+      <S.Container>
+        <S.CardContainer>
+          <HeadingOne>Evenemang</HeadingOne>
+          <S.Card bg="white">
+            <Caption>Erbjudande</Caption>
+            <HeadingTwo>{data.firstCard.title}</HeadingTwo>
+            <BodyNormal>{data.firstCard.bodyText}</BodyNormal>
+          </S.Card>
+          <S.Card bg="green">
+            <Caption>Erbjudande</Caption>
+            <HeadingTwo>{data.secondCard.title}</HeadingTwo>
+            <BodyNormal>{data.secondCard.bodyText}</BodyNormal>
+          </S.Card>
+        </S.CardContainer>
+      </S.Container>
     </>
   );
 };
@@ -20,7 +54,11 @@ const Nyheter: NextPage<Props> = ({ globalSettings }) => {
 export const getServerSideProps = async () => {
   const globalSettings = await getGlobalSettings();
 
-  return { props: { globalSettings } };
+  const data = await client.fetch(`
+    *[_type == "newsPage" && !(_id in path("drafts.**"))][0]
+  `);
+
+  return { props: { globalSettings, data } };
 };
 
 export default Nyheter;
