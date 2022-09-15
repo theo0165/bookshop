75dc76dee06d922429de594fb31de481dea3941a
Theo Sandell <theo01sandell@gmail.com>
2022-09-15 09:48:03 +0200

    Mobile nav

--- packages/client/components/Header.tsx
+++ packages/client/components/Header.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
+import {
+  AiOutlineClose,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineMail,
+  AiOutlineMenu,
+} from "react-icons/ai";
 import GlobalSettings from "../types/GlobalSettings";
 import * as S from "./styled/Header.styles";
 import HeadingThree from "./styled/texts/HeadingThree";
@@ -12,6 +19,7 @@ interface Props {
 
 const Header: FC<Props> = ({ settings, offset }) => {
   const [isSticky, setIsSticky] = useState(false);
+  const [showMobileNav, setShowMobileNav] = useState(false);
   const router = useRouter();
 
   const handleScroll = () => {
@@ -32,43 +40,96 @@ const Header: FC<Props> = ({ settings, offset }) => {
     };
   }, []);
 
+  const toggleMobileNav = () => {
+    setShowMobileNav(!showMobileNav);
+  };
+
   return (
-    <S.HeaderContainer sticky={isSticky}>
-      <S.HeaderInner>
-        <Link href="/">
-          <a>
-            <S.Logo src={settings.logo}></S.Logo>
-          </a>
-        </Link>
-        <S.nav>
-          <S.navList>
-            <S.navItem selected={router.asPath === "/"}>
-              <Link href="/">
-                <a>
-                  <HeadingThree>Hem</HeadingThree>
-                </a>
-              </Link>
-            </S.navItem>
+    <>
+      <S.HeaderContainer sticky={isSticky}>
+        <S.HeaderInner>
+          <Link href="/">
+            <a>
+              <S.Logo src={settings.logo}></S.Logo>
+            </a>
+          </Link>
+          <S.nav>
+            <S.navList>
+              <S.navItem selected={router.asPath === "/"}>
+                <Link href="/">
+                  <a>
+                    <HeadingThree>Hem</HeadingThree>
+                  </a>
+                </Link>
+              </S.navItem>
 
-            <S.navItem selected={router.asPath === "/nyheter"}>
-              <Link href="/nyheter">
-                <a>
-                  <HeadingThree>Nyheter/Evenemang</HeadingThree>
-                </a>
-              </Link>
-            </S.navItem>
+              <S.navItem selected={router.asPath === "/nyheter"}>
+                <Link href="/nyheter">
+                  <a>
+                    <HeadingThree>Nyheter/Evenemang</HeadingThree>
+                  </a>
+                </Link>
+              </S.navItem>
 
-            <S.navItem selected={router.asPath === "/om"}>
-              <Link href="/om">
-                <a>
-                  <HeadingThree>Om oss</HeadingThree>
-                </a>
-              </Link>
-            </S.navItem>
-          </S.navList>
-        </S.nav>
-      </S.HeaderInner>
-    </S.HeaderContainer>
+              <S.navItem selected={router.asPath === "/om"}>
+                <Link href="/om">
+                  <a>
+                    <HeadingThree>Om oss</HeadingThree>
+                  </a>
+                </Link>
+              </S.navItem>
+            </S.navList>
+            <S.HamburgerWrapper onClick={toggleMobileNav}>
+              <AiOutlineMenu />
+            </S.HamburgerWrapper>
+          </S.nav>
+        </S.HeaderInner>
+      </S.HeaderContainer>
+      <S.MobileNav show={showMobileNav}>
+        <S.MobileNavCloseWrapper onClick={toggleMobileNav}>
+          <AiOutlineClose />
+        </S.MobileNavCloseWrapper>
+        <S.MobileNavList>
+          <S.MobileNavItem selected={router.asPath === "/"}>
+            <Link href="/">
+              <a>
+                <HeadingThree>Hem</HeadingThree>
+              </a>
+            </Link>
+          </S.MobileNavItem>
+
+          <S.MobileNavItem selected={router.asPath === "/nyheter"}>
+            <Link href="/nyheter">
+              <a>
+                <HeadingThree>Nyheter/Evenemang</HeadingThree>
+              </a>
+            </Link>
+          </S.MobileNavItem>
+
+          <S.MobileNavItem selected={router.asPath === "/om"}>
+            <Link href="/om">
+              <a>
+                <HeadingThree>Om Oss</HeadingThree>
+              </a>
+            </Link>
+          </S.MobileNavItem>
+        </S.MobileNavList>
+
+        <S.SocialWrapper>
+          <S.SocialLink>
+            <AiOutlineMail />
+          </S.SocialLink>
+
+          <S.SocialLink>
+            <AiOutlineInstagram />
+          </S.SocialLink>
+
+          <S.SocialLink>
+            <AiOutlineFacebook />
+          </S.SocialLink>
+        </S.SocialWrapper>
+      </S.MobileNav>
+    </>
   );
 };
 
--- packages/client/components/styled/Header.styles.ts
+++ packages/client/components/styled/Header.styles.ts
@@ -1,15 +1,20 @@
 import styled, { css } from "styled-components";
 
+//break at 800px
+
 export const HeaderContainer = styled.header<{ sticky?: boolean }>`
   width: 100%;
   z-index: 999999;
   background: ${(props) => props.theme.textWhite};
+  padding: 0 32px;
 
   ${(props) =>
     props.sticky &&
     css`
-      position: sticky;
-      top: 0;
+      @media screen and (min-width: 834px) {
+        position: sticky;
+        top: 0;
+      }
     `}
 `;
 
@@ -29,12 +34,21 @@ export const Logo = styled.img`
 export const nav = styled.nav`
   align-self: end;
   margin-bottom: 7px;
+
+  @media screen and (max-width: 800px) {
+    align-self: center;
+    margin-bottom: 0;
+  }
 `;
 
 export const navList = styled.ul`
   margin: 0;
   padding: 0;
   list-style-type: none;
+
+  @media screen and (max-width: 800px) {
+    display: none;
+  }
 `;
 
 export const navItem = styled.li<{ selected: boolean }>`
@@ -59,5 +73,83 @@ export const navItem = styled.li<{ selected: boolean }>`
 
   &:hover {
     border-color: ${(props) => props.theme.orange};
+  }
+`;
+
+export const MobileNav = styled.nav<{ show?: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: ${(props) => props.theme.primaryGreen};
+  color: white;
+  transform: translateX(100%);
+  transition: transform 0.5s ease-in-out;
+
+  ${(props) =>
+    !props.show &&
+    css`
+      transform: translateX(0);
+    `}
+`;
+
+export const MobileNavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 48px;
+
+  position: absolute;
+  top: 100px;
+  left: 16px;
+`;
+
+export const MobileNavItem = styled.li<{ selected?: boolean }>`
+  display: inline-block;
+
+  h3 {
+    margin: 0;
+  }
+`;
+
+export const SocialWrapper = styled.div`
+  position: absolute;
+  bottom: 32px;
+  left: 16px;
+
+  display: flex;
+  column-gap: 20px;
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const SocialLink = styled.a``;
+
+export const MobileNavCloseWrapper = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 16px;
+  width: 24px;
+  height: 24px;
+`;
+
+export const HamburgerWrapper = styled.div`
+  display: none;
+  width: 24px;
+  height: 16px;
+
+  @media screen and (max-width: 800px) {
+    display: inline-block;
+  }
+
+  svg {
+    width: 24px;
   }
 `;
