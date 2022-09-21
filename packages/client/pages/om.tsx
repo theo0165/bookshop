8ce784817c48238fc00d9a356ac1dc0f084758fa
JonssonJohannae <johanna_jonson@hotmail.com>
2022-09-21 15:16:55 +0200

    Adjusting footer

--- packages/client/pages/om.tsx
+++ packages/client/pages/om.tsx
@@ -113,8 +113,8 @@ const AboutPage: NextPage<Props> = ({ data, globalSettings }) => {
             <ImageChild src={data.imageThird} />
           </ImageWrapper>
         </CardWrapper>
-        <Footer settings={globalSettings} />
       </BodyContainer>
+      <Footer settings={globalSettings} />
     </>
   );
 };
