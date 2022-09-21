3f6d7b066e5b455c7245558bd6a113991df785d7
Theo Sandell <theo01sandell@gmail.com>
2022-09-21 09:15:33 +0200

    Fix container opening tag

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,19 +1,19 @@
-import type { NextPage } from 'next';
-import BodyNormal from '../components/styled/texts/BodyNormal';
-import HeadingOne from '../components/styled/texts/HeadingOne';
-import UnderlineButtonText from '../components/styled/texts/UnderlineButtonText';
-import client from '../helpers/sanity';
-import * as S from '../components/styled/Homepage.styled';
-import Card from '../components/Card';
-import CardType from '../types/Card';
-import getGlobalSettings from '../helpers/getGlobalSettings';
-import GlobalSettings from '../types/GlobalSettings';
-import Header from '../components/Header';
-import TopInfoBar from '../components/TopInfoBar';
-import { useRef } from 'react';
-import InstagramImage from '../types/InstagramImage';
-import DisplayOne from '../components/styled/texts/DisplayOne';
-import Footer from '../components/Footer';
+import type { NextPage } from "next";
+import BodyNormal from "../components/styled/texts/BodyNormal";
+import HeadingOne from "../components/styled/texts/HeadingOne";
+import UnderlineButtonText from "../components/styled/texts/UnderlineButtonText";
+import client from "../helpers/sanity";
+import * as S from "../components/styled/Homepage.styled";
+import Card from "../components/Card";
+import CardType from "../types/Card";
+import getGlobalSettings from "../helpers/getGlobalSettings";
+import GlobalSettings from "../types/GlobalSettings";
+import Header from "../components/Header";
+import TopInfoBar from "../components/TopInfoBar";
+import { useRef } from "react";
+import InstagramImage from "../types/InstagramImage";
+import DisplayOne from "../components/styled/texts/DisplayOne";
+import Footer from "../components/Footer";
 
 interface Props {
   data: {
@@ -58,21 +58,22 @@ const Home: NextPage<Props> = ({ data, globalSettings, instagram }) => {
               <Card data={card} key={`card-${card._key}`} />
             ))}
         </S.CardsContainer>
-      </S.Container>
 
-      <S.InstagramContainer>
-        <DisplayOne>Instagram</DisplayOne>
-        <S.InstagramImageContainer>
-          {instagram.data &&
-            instagram.data.slice(0, 7).map((instagramImage: InstagramImage) => (
-              <a key={instagramImage.id} href={instagramImage.permalink}>
-                <S.InstaImage src={instagramImage.media_url} />
-              </a>
-            ))}
-        </S.InstagramImageContainer>
-      </S.InstagramContainer>
+        <S.InstagramContainer>
+          <DisplayOne>Instagram</DisplayOne>
+          <S.InstagramImageContainer>
+            {instagram.data &&
+              instagram.data
+                .slice(0, 7)
+                .map((instagramImage: InstagramImage) => (
+                  <a key={instagramImage.id} href={instagramImage.permalink}>
+                    <S.InstaImage src={instagramImage.media_url} />
+                  </a>
+                ))}
+          </S.InstagramImageContainer>
+        </S.InstagramContainer>
+      </S.Container>
       <Footer settings={globalSettings} />
-      </S.MainContainer>
     </>
   );
 };
