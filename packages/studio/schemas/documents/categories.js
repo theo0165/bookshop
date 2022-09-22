0620d74399f0345674e8ad63521b2e772d09a470
Theo Sandell <theo01sandell@gmail.com>
2022-09-22 10:19:52 +0200

    Add icons, remove duplicate homepage settings

--- packages/studio/schemas/documents/categories.js
+++ packages/studio/schemas/documents/categories.js
@@ -1,7 +1,10 @@
+import { AiOutlineAppstore } from "react-icons/ai";
+
 export default {
   name: "category",
   type: "document",
   title: "Kategorier",
+  icon: AiOutlineAppstore,
   fields: [
     {
       name: "title",
